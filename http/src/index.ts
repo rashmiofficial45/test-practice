d108d75de1c00812c3ca72ffbd5eb9b6fff37575
Rashmi Ranjan Parija <126317142+rashmiofficial45@users.noreply.github.com>
2026-01-01 05:21:42 +0530

    feat: Add user authentication context to Express Request, update JWT payload, and implement `/auth/me` endpoint.

--- http/src/index.ts
+++ http/src/index.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose"
 import { User, Class, Attendance } from "./models"
 import jwt from "jsonwebtoken"
 import { signUpSchema, signInSchema, classSchema } from "./types"
+import { authMiddleware } from "./middleware"
 const app = express()
 const port = process.env.PORT || 4000
 
@@ -86,7 +87,7 @@ app.post("/auth/login",async(req,res)=> {
       )
       return
     }
-    const token = jwt.sign({email: data.email, password: data.password }, process.env.JWT_SECRET!)
+    const token = jwt.sign({userId: user._id, role:user.role }, process.env.JWT_SECRET!)
     res.status(200).json({
       "success": true,
       "data": {
@@ -102,6 +103,40 @@ app.post("/auth/login",async(req,res)=> {
   }
 })
 
+
+app.get("/auth/me", authMiddleware, async(req,res)=> {
+  try {
+    const user = await User.findOne({
+      _id: req.userId
+    })
+    if(!user){
+      res.status(401).json(
+        {
+          "success": false,
+          "error": "Unauthorized"
+        }
+      )
+      return
+    }
+    res.status(200).json(
+      {
+        "success": true,
+        "data": {
+          "_id": user._id,
+          "email": user.email,
+          "name": user.name,
+          "role": user.role
+        }
+      }
+    )
+  } catch (error) {
+    console.error("Error: ", error)
+    res.status(500).json({
+      "success": false,
+      "error": "Internal Server Error"
+    })
+  }
+})
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
